b04d834e9c88ec6de5742e9b8556488c9316a378
Peiqi333645 <1610419428@qq.com>
2026-09-04 10:13:57 +0800

    fix: add neutral prearm checks and stable stick filtering

--- demo/scripts/soccer_controller.gd
+++ demo/scripts/soccer_controller.gd
@@ -6,6 +6,9 @@ var drone: DroneBody
 var game: Node
 var armed := false
 var throttle_smoothed := 0.0
+var roll_filtered := 0.0
+var pitch_filtered := 0.0
+var yaw_filtered := 0.0
 func _ready() -> void:
 	drone = get_node(drone_path) as DroneBody
 	game = get_node(game_path)
@@ -21,12 +24,20 @@ func _process(delta: float) -> void:
 	var throttle: float = clampf((InputProfile.value(&"throttle") + 1.0) * 0.5, 0.0, 1.0)
 	var response: float = 1.0 - exp(-lerpf(4.0, 16.0, 1.0 - throttle_smoothing) * delta)
 	throttle_smoothed = lerpf(throttle_smoothed, _throttle_curve(throttle), response)
+	var input_response: float = 1.0 - exp(-28.0 * delta)
+	roll_filtered = lerpf(roll_filtered, InputProfile.value(&"roll"), input_response)
+	pitch_filtered = lerpf(pitch_filtered, InputProfile.value(&"pitch"), input_response)
+	yaw_filtered = lerpf(yaw_filtered, InputProfile.value(&"yaw"), input_response)
+	if not is_finite(roll_filtered) or not is_finite(pitch_filtered) or not is_finite(yaw_filtered):
+		game.set_status("输入异常，已自动锁定")
+		disarm()
+		return
 	var roll_values: Dictionary = InputProfile.axis_rates["roll"]
 	var pitch_values: Dictionary = InputProfile.axis_rates["pitch"]
 	var yaw_values: Dictionary = InputProfile.axis_rates["yaw"]
-	var roll_rate: float = _bf_rate(InputProfile.value(&"roll"), float(roll_values.rc), float(roll_values.super), float(roll_values.expo))
-	var pitch_rate: float = _bf_rate(InputProfile.value(&"pitch"), float(pitch_values.rc), float(pitch_values.super), float(pitch_values.expo))
-	var yaw_rate: float = _bf_rate(InputProfile.value(&"yaw"), float(yaw_values.rc), float(yaw_values.super), float(yaw_values.expo))
+	var roll_rate: float = _bf_rate(roll_filtered, float(roll_values.rc), float(roll_values.super), float(roll_values.expo))
+	var pitch_rate: float = _bf_rate(pitch_filtered, float(pitch_values.rc), float(pitch_values.super), float(pitch_values.expo))
+	var yaw_rate: float = _bf_rate(yaw_filtered, float(yaw_values.rc), float(yaw_values.super), float(yaw_values.expo))
 	drone.set_rate_setpoint(roll_rate, pitch_rate, yaw_rate, throttle_smoothed)
 	if drone.global_position.y < -2.0 or drone.global_position.y > 60.0: game.reset_drone()
 func _bf_rate(stick: float, rc_rate: float, super_rate: float, rate_expo: float) -> float:
@@ -66,9 +77,18 @@ func apply_profile() -> void:
 func arm() -> void:
 	if armed or game.input_blocked: return
 	var throttle: float = (InputProfile.value(&"throttle") + 1.0) * 0.5
+	var roll_input: float = InputProfile.value(&"roll")
+	var pitch_input: float = InputProfile.value(&"pitch")
+	var yaw_input: float = InputProfile.value(&"yaw")
 	if throttle > 0.08:
 		game.set_status("无法解锁：请先把油门杆拉到最低")
 		return
+	if absf(roll_input) > 0.12 or absf(pitch_input) > 0.12 or absf(yaw_input) > 0.12:
+		game.set_status("无法解锁：摇杆未居中，请重新校准 R/P/Y")
+		return
+	roll_filtered = 0.0
+	pitch_filtered = 0.0
+	yaw_filtered = 0.0
 	drone.sleeping = false
 	drone.arm()
 	armed = true
@@ -79,6 +99,9 @@ func disarm() -> void:
 	drone.disarm()
 	armed = false
 	throttle_smoothed = 0.0
+	roll_filtered = 0.0
+	pitch_filtered = 0.0
+	yaw_filtered = 0.0
 	game.set_status("已锁定")
 func force_disarm() -> void:
 	disarm()
